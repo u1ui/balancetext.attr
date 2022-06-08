3c97db06063b1699d217749f2616bfa99a900076
Tobias Buschor <tobias.buschor@shwups.ch>
2022-06-09 01:22:40 +0200

    initial

---
+++ balancetext.js
@@ -0,0 +1,43 @@
+import {SelectorObserver} from 'https://cdn.jsdelivr.net/gh/u1ui/SelectorObserver.js@3.0.1/SelectorObserver.min.js'
+
+const resizeObs = new ResizeObserver(entries => {
+    for (const entry of entries) balanceText(entry.target);
+});
+
+const selObs = new SelectorObserver({
+    on(el){
+        if (el.isContentEditable) return;
+        resizeObs.observe(el);
+        balanceText(el);
+    },
+    off(el){
+        resizeObs.unobserve(el);
+    }
+});
+selObs.observe('[u1-balancetext]');
+
+
+function balanceAll(){
+    for (let el of selObs.targets) balanceText(el);
+}
+document.addEventListener('DOMContentLoaded',balanceAll, {once:true});
+document.addEventListener('load',balanceAll, {once:true});
+
+
+function balanceText(el){
+    require('https://cdn.jsdelivr.net/npm/balance-text@3.3.1/balancetext.min.js').then(balText=>{
+        balText(el);
+        balanceText = balText;
+    });
+}
+
+/*
+require('https://cdn.jsdelivr.net/npm/balance-text@3.3.1/balancetext.min.js').then(balText=>{
+    balText('[u1-balancetext]', {watch:true});
+});
+*/
+async function require(path){
+    window.module = {exports:{}};
+    await import(path);
+    return module.exports;
+}
